6e8a4dfc9c77ca3934867c0fa7f62f31a90a4f58
IanConroy <ianmylesconroy@gmail.com>
2025-10-29 10:08:50 -0400

    added canvas. testing

--- javascript.js
+++ javascript.js
@@ -1,14 +1,5 @@
-document.getElementById("div1").style.color = "#444444";
-document.getElementById("div1").style.margin = "40px";
-document.getElementById("div1").style.overflow = "auto";
-document.getElementById("div1").style.flex = "auto";
 
-function makeGrid() {
-    const container = document.getElementById("container");
-    const makeDiv = document.createElement("div");
-    document.getElementById("container");
-    for (let i = 0; i < 100; i++) {
-    makeDiv.fillStyle = "white";
-    makeDiv.fillRect(5, 5, 5, 5)
-}
-}
+const canvas = document.getElementById("canTest");
+    const ctx = canTest.getContext("2d");
+    ctx.fillStyle = "#939394ff";
+    ctx.fillRect(10, 10, 10, 10);
