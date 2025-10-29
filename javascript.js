be863cd533c1172d01170877a9715d751f695266
IanConroy <ianmylesconroy@gmail.com>
2025-10-29 19:10:45 -0400

    minor changes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,28 @@
+const canvas = document.getElementById("canTest");
+const ctx = canTest.getContext("2d");
+/**const grid = ctx.makeGrid("repeat"); **/
+    ctx.onload = makeGrid("repeat"); {
+    ctx.fillStyle = "#939394ff";
+    ctx.fillRect(10, 10, ctx.width, ctx.height);
+    ctx.createPattern(ctx, "repeat");
+    }
 
+/**
+function makeGrid() {
 const canvas = document.getElementById("canTest");
     const ctx = canTest.getContext("2d");
+    for (let i = 0; i < 100; i++) {
+    ctx.beginPath();
     ctx.fillStyle = "#939394ff";
     ctx.fillRect(10, 10, 10, 10);
+    ctx.fill();
+    }
+}
+
+
+ctx.addEventListener("load", () => {
+    const grid = ctx.makeGrid("repeat");
+    ctx.fillStyle = grid;
+    ctx.fillRect(10, 10, ctx.width, ctx.height);
+})
+    **/
